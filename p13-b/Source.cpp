57158faa0aa5627d25623de1d9a434abf28ec682
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-12-07 05:34:16 -0500

    p13b-3 completed

--- p13-b/Source.cpp
+++ p13-b/Source.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 bool isLeapYear(int year);
 int daysInMonth(int month, int year);
+int dayOfWeek(int month, int day, int year);
 
 int main() {
-	int month, year;
-	string monthName;
+	int month, year, weekday, day;
+	string monthName, dayName;
 	do {
-		cout << "Enter a month and year or Q to quit: ";
-		cin >> month >> year;
+		cout << "Enter a date or Q to quit: ";
+		cin >> month >> day >> year;
 
 		//CHECKS WHETHER INPUT IS Q OR q
 		if (year == 'Q' || year == 'q') {
@@ -36,8 +38,22 @@ int main() {
 			}
 
 			//CHECKS THE AMOUNT OF DAYS IN THE MONTH
-			int days = daysInMonth(month, year);
-			cout << monthName << " " << year << " has " << days << " days.\n";
+			          //int days = daysInMonth(month, year);
+			weekday = dayOfWeek(month, day, year);
+
+			//CHECKS THE DAY NAME
+			switch (weekday) {
+			case 0: dayName = "Saturday"; break;
+			case 1: dayName = "Sunday"; break;
+			case 2: dayName = "Monday"; break;
+			case 3: dayName = "Tuesday"; break;
+			case 4: dayName = "Wednesday"; break;
+			case 5: dayName = "Thursday"; break;
+			case 6: dayName = "Friday"; break;
+			}
+
+
+			cout << dayName << ", " << monthName << " " << day << ", " << year << "\n";
 
 		}
 	} while (!cin.fail());
@@ -45,7 +61,7 @@ int main() {
 	return 0;
 }
 
-
+//FUNCTION DEFINITIONS
 /*isLeapYear - determines whether a given year is a leap year under the Gregorian calendar.
 @param year the year; expected to be >=1582
 @return true if the year is a leap year; false otherwise
@@ -109,4 +125,31 @@ int daysInMonth(int month, int year) {
 	default: break;
 	}
 
+}
+
+
+/*dayOfWeek - Computes the weekday of a given date.
+@param year the year
+@param month the month(1 = January... 12 = December)
+@param day the day of the month
+@return the weekday (0 = Saturday... 6 = Friday)
+*/
+int dayOfWeek(int month, int day, int year) {
+	int weekday;
+	int q = day;
+	int m = month;
+	if (m < 3) {
+		m = m + 12;
+	}
+	int y = year;
+	if (m > 12) {
+		y = y - 1;
+	}
+
+	cout << q << " " << m << " " << y << " \n";
+
+
+	weekday = ((y + (y/4)-(y/100)+(y/400)+((13*m)+8)/5+q)+1)%7;
+	cout << weekday << "\n";
+	return weekday;
 }
